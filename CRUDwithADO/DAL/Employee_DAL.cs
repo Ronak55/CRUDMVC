3973dc18e990a838083ba874b2957431ae28aae1
Ronak55 <rj.rjain567@gmail.com>
2024-07-16 22:53:00 +0530

    initial commit

---
+++ CRUDwithADO/DAL/Employee_DAL.cs
@@ -0,0 +1,154 @@
+﻿using CRUDwithADO.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Reflection;
+
+namespace CRUDwithADO.DAL
+{
+    public class Employee_DAL
+    {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+
+        public static IConfiguration Configuration { get; set; }
+
+        public string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            return Configuration.GetConnectionString("DefaultConnection");
+
+
+        }
+
+        public List<Employee> GetAll()
+        {
+            List<Employee> employeeList = new List<Employee>();
+
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = System.Data.CommandType.StoredProcedure;
+                _command.CommandText = "dbo.usp_Get_Employees";
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.Id = Convert.ToInt32(dr["Id"]);
+                    employee.FirstName = dr["FirstName"].ToString();
+                    employee.LastName = dr["LastName"].ToString();
+                    employee.DateOfBirth = DateOnly.FromDateTime(Convert.ToDateTime(dr["DateOfBirth"]));
+                    employee.Email = dr["Email"].ToString();
+                    employee.Salary = Convert.ToDouble(dr["Salary"]);
+
+                    employeeList.Add(employee);
+                }
+                _connection.Close();
+            }
+
+            return employeeList;
+        }
+
+        public bool Insert(Employee model)
+        {
+            int id = 0;
+
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+
+                _command = _connection.CreateCommand();
+                _command.CommandType = System.Data.CommandType.StoredProcedure;
+                _command.CommandText = "dbo.usp_Insert_Employee";
+                _command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                _command.Parameters.AddWithValue("@LastName", model.LastName);
+                _command.Parameters.AddWithValue("@DateOfBirth", model.DateOfBirth);
+                _command.Parameters.AddWithValue("@Email", model.Email);
+                _command.Parameters.AddWithValue("@Salary", model.Salary);
+
+                _connection.Open();
+                id = _command.ExecuteNonQuery();
+                _connection.Close();
+
+            }
+            return id > 0 ? true : false;
+        }
+
+        public Employee GetById(int id)
+        {
+            Employee employee = new Employee();
+
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = System.Data.CommandType.StoredProcedure;
+                _command.CommandText = "dbo.usp_Get_EmployeeById";
+                _command.Parameters.AddWithValue("@Id", id);
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    employee.Id = Convert.ToInt32(dr["Id"]);
+                    employee.FirstName = dr["FirstName"].ToString();
+                    employee.LastName = dr["LastName"].ToString();
+                    employee.DateOfBirth = DateOnly.FromDateTime(Convert.ToDateTime(dr["DateOfBirth"]));
+                    employee.Email = dr["Email"].ToString();
+                    employee.Salary = Convert.ToDouble(dr["Salary"]);
+                }
+                _connection.Close();
+            }
+
+            return employee;
+        }
+
+        public bool Update(Employee model)
+        {
+            int id = 0;
+
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+
+                _command = _connection.CreateCommand();
+                _command.CommandType = System.Data.CommandType.StoredProcedure;
+                _command.CommandText = "dbo.usp_Update_Employee";
+                _command.Parameters.AddWithValue("@Id", model.Id);
+                _command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                _command.Parameters.AddWithValue("@LastName", model.LastName);
+                _command.Parameters.AddWithValue("@DateOfBirth", model.DateOfBirth);
+                _command.Parameters.AddWithValue("@Email", model.Email);
+                _command.Parameters.AddWithValue("@Salary", model.Salary);
+
+                _connection.Open();
+                id = _command.ExecuteNonQuery();
+                _connection.Close();
+
+            }
+            return id > 0 ? true : false;
+        }
+
+        public bool Delete(int id)
+        {
+            int deletedRowCount = 0;
+
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+
+                _command = _connection.CreateCommand();
+                _command.CommandType = System.Data.CommandType.StoredProcedure;
+                _command.CommandText = "dbo.usp_Delete_Employee";
+                _command.Parameters.AddWithValue("@Id", id);
+
+                _connection.Open();
+                deletedRowCount = _command.ExecuteNonQuery();
+                _connection.Close();
+
+            }
+            return id > 0 ? true : false;
+
+        }
+
+    }
+}
